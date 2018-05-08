3ad4077a6597cee8d31454ee0cc2c9fe1d4d8ed9
Stallmanifold <stallmanifold@gmail.com>
2018-05-08 21:36:13 +0000

    More test adjustments.

--- rollservice/tests/test_dice_seq.py
+++ rollservice/tests/test_dice_seq.py
@@ -7,7 +7,7 @@ import rest_framework.status  as status
 import rest_framework.reverse as reverse
 
 import hypothesis.extra.django
-import hypothesis.strategies as strategies
+import hypothesis.strategies  as strategies
 
 
 class DiceSeqStrategies:
@@ -46,16 +46,11 @@ class DiceSeqStrategies:
         
         return queryset[index].uuid
         
-
-    @strategies.composite
-    def non_existing_uuid(draw):
-        return dict(uuid = draw(strategies.uuids()), exists = False, valid_uuid = False)
+    non_existing_uuid = strategies.uuids()
 
     @strategies.composite
     def invalid_uuid(draw):
-        return dict(
-            uuid = draw(strategies.text(max_size=100)), exists = False, valid_uuid = False
-        )
+        return draw(strategies.text(max_size=100))
 
 
 class DiceSeqTests(hypothesis.extra.django.TestCase):
@@ -81,17 +76,17 @@ class DiceSeqTests(hypothesis.extra.django.TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
-    def test_dice_seq_by_uuid_with_non_existing_uuid_should_return_not_found(self):
-        uuid = DiceSeqStrategies.non_existing_uuid().example()
-        url = reverse.reverse('dice-seq-by-uuid', kwargs={'uuid': uuid['uuid']})
+    @hypothesis.given(DiceSeqStrategies.non_existing_uuid)
+    def test_dice_seq_by_uuid_with_non_existing_uuid_should_return_not_found(self, uuid):
+        url = reverse.reverse('dice-seq-by-uuid', kwargs={'uuid': uuid})
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
-    def test_dice_seq_by_uuid_with_non_existing_uuid_should_return_not_found(self):
-        uuid = DiceSeqStrategies.invalid_uuid().example()
-        url = reverse.reverse('dice-seq-by-uuid', kwargs={'uuid': uuid['uuid']})
+    @hypothesis.given(DiceSeqStrategies.invalid_uuid())
+    def test_dice_seq_by_uuid_with_invalid_uuid_should_return_bad_request(self, uuid):
+        url = reverse.reverse('dice-seq-by-uuid', args=[uuid])
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
