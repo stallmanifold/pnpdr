c8d2f2754ff67bc6e3628c87d77ce6bdb44aa261
Stallmanifold <stallmanifold@gmail.com>
2018-05-08 09:25:53 +0000

    Add hypothesis test.

--- pnpdr/settings.py
+++ pnpdr/settings.py
@@ -79,6 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+    },
+    'test_db': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../database/test_db.sqlite3'),
     }
 }
 
